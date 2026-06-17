e03e50025c2a6716090ca8191bf149dc565dd13c
Jinwoo <jinw0909@gmail.com>
2026-06-17 22:37:11 +0900

    initial commit

---
+++ src/main/java/com/kqsf/KqsfApplication.java
@@ -0,0 +1,13 @@
+package com.kqsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KqsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KqsfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kqsf/KqsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kqsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KqsfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
